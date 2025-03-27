a5acf389cae8b94c2fd5130ca7080d56e7126a5d
Sergio <srvalle@gmail.com>
2025-03-27 18:37:25 -0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,53 @@ import React from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { Tabs } from './components/Tabs/Tabs';
 
 export const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-10', title: 'Tab 10', content: 'Some text 10' },
+  { id: 'tab-20', title: 'Tab 20', content: 'Some text 20' },
+  { id: 'tab-30', title: 'Tab 30', content: 'Some text 30' },
+  { id: 'tab-40', title: 'Tab 40', content: 'Some text 40' },
 ];
 
-export const App = () => (
-  <div className="section">
-    <h1 className="title">Selected tab is Tab 1</h1>
+export const App = () => {
+  let activeTabId = 'tab-30';
 
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">
-              Tab 1
-            </a>
-          </li>
+  const onTabSelected = (id, content, event) => {
+    event.preventDefault();
 
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">
-              Tab 2
-            </a>
-          </li>
+    // acessar o li. event.target se refere ao elemento <a> que foi clicado.
+    // parentNode é o pai dele, no caso o li, que precisamos para adicionar
+    // a classe 'is-active'.
+    const clickedLi = event.target.parentNode;
+    const selectedTab = id.split('-');
+    const title = document.querySelector('.title');
+    const tabContent = document.querySelector('.block');
+    const removeLiClass = document.querySelectorAll('[data-cy="Tab"]'); // poderia ser 'li'
 
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">
-              Tab 3
-            </a>
-          </li>
-        </ul>
-      </div>
+    title.textContent = `Selected tab is Tab ${selectedTab[1]}`;
+    tabContent.textContent = content;
 
-      <div className="block" data-cy="TabContent">
-        Some text 1
-      </div>
+    removeLiClass.forEach(li => {
+      li.classList.remove('is-active');
+    });
+
+    clickedLi.classList.add('is-active');
+  };
+
+  activeTabId = tabs.find(tab => tab.id === activeTabId)?.id || tabs[0].id;
+  const pageTitle = activeTabId.split('-')[1];
+  // alert(titleActive);
+
+  return (
+    <div className="section">
+      <h1 className="title">Selected tab is Tab {pageTitle}</h1>
+
+      <Tabs
+        tabs={tabs}
+        activeTabId={activeTabId}
+        onTabSelected={onTabSelected}
+      />
     </div>
-  </div>
-);
+  );
+};
